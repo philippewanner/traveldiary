958fab7a10ba235af02d101bb63da18f8d2de8f5
Andreas Heubeck <heuby@gmx.ch>
2016-02-08 14:17:56 +0100

    Initial Commit

---
+++ TravelDiary/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TravelDiary
+//
+//  Created by Andreas Heubeck on 08/02/16.
+//  Copyright © 2016 PTPA. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TravelDiary/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TravelDiary
+//
+//  Created by Andreas Heubeck on 08/02/16.
+//  Copyright © 2016 PTPA. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
