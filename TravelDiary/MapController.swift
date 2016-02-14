7a3488970c7e5f65ac22124e61c0794144468af0
Tobias Rindlisbacher <tobias.rindlisbacher@gmail.com>
2016-02-14 15:01:18 +0100

    Add MapKit

--- TravelDiary/MapController.swift
+++ TravelDiary/MapController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
-class MapController: UIViewController {
+class MapController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView! {
+        didSet {
+            mapView.delegate = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +27,5 @@ class MapController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
