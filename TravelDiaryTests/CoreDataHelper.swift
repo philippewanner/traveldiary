3d305d45665cb7c1b39c9e9cce59208edaacdddc
Andreas Heubeck <heuby@gmx.ch>
2016-02-16 21:44:23 +0100

    Helper for creating inMemory ManagedObjectContext

---
+++ TravelDiaryTests/CoreDataHelper.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataHelper.swift
+//  TravelDiary
+//
+//  Created by Andreas Heubeck on 16/02/16.
+//  Copyright © 2016 PTPA. All rights reserved.
+//
+
+import CoreData
+
+func setUpInMemoryManagedObjectContext() -> NSManagedObjectContext {
+    let managedObjectModel = NSManagedObjectModel.mergedModelFromBundles([NSBundle.mainBundle()])!
+    
+    let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+    
+    do {
+        try persistentStoreCoordinator.addPersistentStoreWithType(NSInMemoryStoreType, configuration: nil, URL: nil, options: nil)
+    } catch {
+        print("Adding in-memory persistent store coordinator failed")
+    }
+    
+    let managedObjectContext = NSManagedObjectContext()
+    managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+    
+    return managedObjectContext
+}
