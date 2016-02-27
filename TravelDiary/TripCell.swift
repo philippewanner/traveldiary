2e244387bebcc2845633103e21286ccb981e877f
Peter K. Mäder <peter.k.maeder@gmail.com>
2016-02-27 16:27:29 +0100

    Extracting trips overview code and create an TableView

---
+++ TravelDiary/TripCell.swift
@@ -0,0 +1,15 @@
+//
+//  TripCell.swift
+//  TravelDiary
+//
+//  Created by Peter K. Mäder on 22/02/16.
+//  Copyright © 2016 PTPA. All rights reserved.
+//
+
+import UIKit
+
+class TripCell: UITableViewCell{
+
+
+
+}
--- TravelDiary/TripsViewController.swift
+++ TravelDiary/TripsViewController.swift
@@ -7,19 +7,22 @@
 //
 
 import UIKit
+import CoreData
 
-class SecondViewController: UIViewController {
+class TripsViewController: UIViewController{
 
+    @IBOutlet weak var trips: UITableView!
+    @IBOutlet var tripsTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tripTableViewCellNib = UINib(nibName: "TripTableViewCell", bundle: nil)
+        trips.registerNib(tripTableViewCellNib, forCellReuseIdentifier: "reuseCell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
