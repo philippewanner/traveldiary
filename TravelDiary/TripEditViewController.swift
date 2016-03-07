7986644bc6185c92991d285a372d212b0a62e34c
Peter K. Mäder <peter.k.maeder@gmail.com>
2016-03-07 11:20:55 +0100

    adding TripEditView

---
+++ TravelDiary/TripEditViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TripEditViewController.swift
+//  TravelDiary
+//
+//  Created by Peter K. Mäder on 07/03/16.
+//  Copyright © 2016 PTPA. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TripEditViewController : UIViewController{
+    
+    private struct Constants{
+        static let tripEditViewNibName = "TripEditView"
+        static let cellReuseIdentifier = "reuseTripTableViewCell"
+        static let localeIdentifier = "de_CH"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+}
